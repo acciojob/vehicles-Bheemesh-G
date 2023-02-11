d191c654266ff5c96e262a20ffa2d2204e0e4172
Bheemesh <gbheemesh888@gmail.com>
2023-02-11 18:25:37 +0530

    first

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,14 +11,30 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
     }
 
-    public void changeGear(int newGear){
+    public Car(String name,boolean isManual)
+    {
+        super(name);
+        this.isManual = isManual;
+    }
 
+    public void changeGear(int newGenar){
+      this.currentGear = newGenar;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
+
+        this.move(newSpeed,newDirection);
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
